a51b2b19df1bfab0134d454857d0b8d30abb7716
YasumiYami <yasumi.yami@gmail.com>
2020-05-05 00:24:34 +0900

    feat: add target blank

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -5,21 +5,21 @@
         <h3 class="title">Social</h3>
         <div class="level">
           <div class="level-item">
-            <a href="https://github.com/yasumi-yami">
+            <a href="https://github.com/yasumi-yami" target="_blank">
               <figure class="image is-64x64">
                 <img src="@/assets/github.png"/>
               </figure>
             </a>
           </div>
           <div class="level-item">
-            <a href="https://twitter.com/YamiYasumi">
+            <a href="https://twitter.com/YamiYasumi" target="_blank">
               <figure class="image is-64x64">
                 <img src="@/assets/twitter.png"/>
               </figure>
             </a>
           </div>
           <div class="level-item">
-            <a href="https://www.youtube.com/channel/UCkLqZyQsxTSoqpAL51418cQ">
+            <a href="https://www.youtube.com/channel/UCkLqZyQsxTSoqpAL51418cQ" target="_blank">
               <figure class="image is-64x64">
                 <img src="@/assets/youtube.png"/>
               </figure>
